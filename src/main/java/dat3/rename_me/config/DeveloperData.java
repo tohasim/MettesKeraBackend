522a0ef2d88ebf5a0643a66d956da478b3b72888
Tohasim <toreh.simonsen@live.dk>
2023-11-30 16:11:02 +0100

    Fixed categories and added new test data with "real" products

--- src/main/java/dat3/rename_me/config/DeveloperData.java
+++ src/main/java/dat3/rename_me/config/DeveloperData.java
@@ -27,31 +27,8 @@ public class DeveloperData implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
-        List<Category> categories = CategoryTestDataFactory.generateTestCategories();
-        categoryRepository.saveAll(categories);
-
-        List<Product> products = ProductTestDataFactory.generateTestProducts();
-        productRepository.saveAll(products);
-        Product.ProductImage image1 = Product.ProductImage.builder()
-                .imageUrl("https://grene-prod-omni.azureedge.net/catalog/159479-1-513222-002_pack_a.png?format=webp&width=640&height=853&quality=80&mode=crop&bgcolor=faf7f0")
-                .build();
-
-        Product.ProductImage image2 = Product.ProductImage.builder()
-                .imageUrl("https://juliedamhus.dk/media/products/unnamed-19-1.jpg")
-                .build();
-
-        Product product1 = Product.builder()
-                .name("Sej skål")
-                .category("Skål")
-                .description("Lækker skål med sejt design")
-                .price(350.00)
-                .stock(10)
-                .images(Arrays.asList(image1, image2))
-                .build();
-        image1.setProduct(product1);
-        image2.setProduct(product1);
-        productRepository.save(product1);
+        ProductTestDataFactory factory = new ProductTestDataFactory(categoryRepository, productRepository);
+        factory.generateTestProducts();
 
     }
 }
--- src/main/java/dat3/rename_me/entity/Product.java
+++ src/main/java/dat3/rename_me/entity/Product.java
@@ -20,16 +20,29 @@ public class Product {
 
     @Column(unique = true)
     private String name;
-    private String category;
     private String description;
     private double price;
     private int stock;
 
-    // Remove imageUrl from Product entity
+    @ManyToOne
+    private Category category;
 
+    // Remove imageUrl from Product entity
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "product")
     private List<ProductImage> images;
 
+    public Product(String name, String description, double price, int stock, Category category, List<ProductImage> images) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.category = category;
+        this.images = images;
+        category.addProduct(this);
+        for (ProductImage image : images) {
+            image.setProduct(this);
+        }
+    }
 
     @Getter
     @Setter
@@ -48,5 +61,4 @@ public class Product {
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
     }
-
 }
